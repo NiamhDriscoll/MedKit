40a4a9afce213fa82cb9b8c427bc259ec4e72e4a
Niamh Driscoll <niamhmolson@outlook.com>
2025-07-04 14:05:40 -0700

    Add files via upload

---
+++ cmake-build-debug/main.py
@@ -0,0 +1,21 @@
+import json
+import time
+from datetime import datetime
+time.time()
+print(time.time())
+
+
+time.sleep(5)
+while True:
+    with open('time.json', 'r') as medication_time:
+        data = json.load(medication_time)
+    with open('time.json', 'r') as medication_time:
+         data = json.load(medication_time)
+    time_only = datetime.now().strftime("%H:%M")
+    day = datetime.now().strftime("%A")
+    time_key = f"{day}Time"
+    med_time = data[time_key]
+    if time_only == med_time:
+        print(f"time to take medication: ")
+        print(data)
+
